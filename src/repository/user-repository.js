1bc79d8b588e90d28abca253af95102a9ab3be13
AKcode07 <ashwinkumavat07@gmail.com>
2024-06-08 14:31:07 +0530

    7th June 2024

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -30,17 +30,17 @@ class UserRepository {
         }
     }
 
-    // async getById(userId) {
-    //     try {
-    //         const user = await User.findByPk(userId, {
-    //             attributes: ['email', 'id']
-    //         });
-    //         return user;
-    //     } catch (error) {
-    //         console.log("Something went wrong on repository layer");
-    //         throw error;
-    //     }
-    // }
+    async getById(userId) {
+        try {
+            const user = await User.findByPk(userId, {
+                attributes: ['email', 'id']
+            });
+            return user;
+        } catch (error) {
+            console.log("Something went wrong on repository layer");
+            throw error;
+        }
+    }
 
     // async getByEmail(userEmail) {
     //     try {
